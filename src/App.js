ef7f2f16ee3258b95f89d40b12305fedc9ed330f
devRabbani <dev.golamrabbani@gmail.com>
2022-09-26 02:03:02 +0530

    making temp frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="/cwbclub.svg" className="App-logo" alt="logo" />
+        <p>Your coding club's platform is under process.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://canwebe.tech"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Know about CanWeBe!
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
