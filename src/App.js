17ada009d77c99dee5d468194b41147b4143c239
devRabbani <dev.golamrabbani@gmail.com>
2022-09-27 20:00:39 +0530

    Adding header tag

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Helllo There</h1>
         <img src="/cwbclub.svg" className="App-logo" alt="logo" />
         <p>Your coding club's platform is under process.</p>
         <a
